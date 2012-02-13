cba3882ae8160cd8a62e7561f19ed5a7322627df
Costa Walcott <costa@scvngr.com>
2012-02-13 16:24:18 -0500

    Ported DefaultGridSampler

--- ZXingObjC/common/DefaultGridSampler.h
+++ ZXingObjC/common/DefaultGridSampler.h
@@ -4,6 +4,8 @@
  * @author Sean Owen
  */
 
+@class BitMatrix, PerspectiveTransform;
+
 @interface DefaultGridSampler : GridSampler
 
 - (BitMatrix *) sampleGrid:(BitMatrix *)image dimensionX:(int)dimensionX dimensionY:(int)dimensionY p1ToX:(float)p1ToX p1ToY:(float)p1ToY p2ToX:(float)p2ToX p2ToY:(float)p2ToY p3ToX:(float)p3ToX p3ToY:(float)p3ToY p4ToX:(float)p4ToX p4ToY:(float)p4ToY p1FromX:(float)p1FromX p1FromY:(float)p1FromY p2FromX:(float)p2FromX p2FromY:(float)p2FromY p3FromX:(float)p3FromX p3FromY:(float)p3FromY p4FromX:(float)p4FromX p4FromY:(float)p4FromY;
