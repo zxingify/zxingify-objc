543f5058ca6cae62da6093c198ddefe606c4fb92
Costa Walcott <costa@scvngr.com>
2012-02-15 12:19:48 -0500

    Ported SMSMMSResultParser

--- ZXingObjC/client/result/SMSMMSResultParser.m
+++ ZXingObjC/client/result/SMSMMSResultParser.m
@@ -2,13 +2,13 @@
 #import "SMSMMSResultParser.h"
 #import "SMSParsedResult.h"
 
-@implementation SMSMMSResultParser
+@interface SMSMMSResultParser ()
 
-- (id) init {
-  if (self = [super init]) {
-  }
-  return self;
-}
++ (void) addNumberVia:(NSMutableArray *)numbers vias:(NSMutableArray *)vias numberPart:(NSString *)numberPart;
+
+@end
+
+@implementation SMSMMSResultParser
 
 + (SMSParsedResult *) parse:(Result *)result {
   NSString * rawText = [result text];
@@ -18,52 +18,54 @@
   if (!([rawText hasPrefix:@"sms:"] || [rawText hasPrefix:@"SMS:"] || [rawText hasPrefix:@"mms:"] || [rawText hasPrefix:@"MMS:"])) {
     return nil;
   }
+
   NSMutableDictionary * nameValuePairs = [self parseNameValuePairs:rawText];
   NSString * subject = nil;
   NSString * body = nil;
   BOOL querySyntax = NO;
-  if (nameValuePairs != nil && ![nameValuePairs empty]) {
-    subject = (NSString *)[nameValuePairs objectForKey:@"subject"];
-    body = (NSString *)[nameValuePairs objectForKey:@"body"];
+  if (nameValuePairs != nil && [nameValuePairs count] > 0) {
+    subject = [nameValuePairs objectForKey:@"subject"];
+    body = [nameValuePairs objectForKey:@"body"];
     querySyntax = YES;
   }
-  int queryStart = [rawText rangeOfString:'?' param1:4];
+
+  int queryStart = [rawText rangeOfString:@"?" options:NSLiteralSearch range:NSMakeRange(4, [rawText length] - 4)].location;
   NSString * smsURIWithoutQuery;
   if (queryStart < 0 || !querySyntax) {
     smsURIWithoutQuery = [rawText substringFromIndex:4];
+  } else {
+    smsURIWithoutQuery = [rawText substringWithRange:NSMakeRange(4, [rawText length] - queryStart)];
   }
-   else {
-    smsURIWithoutQuery = [rawText substringFromIndex:4 param1:queryStart];
-  }
+
   int lastComma = -1;
   int comma;
-  NSMutableArray * numbers = [[[NSMutableArray alloc] init:1] autorelease];
-  NSMutableArray * vias = [[[NSMutableArray alloc] init:1] autorelease];
-
-  while ((comma = [smsURIWithoutQuery rangeOfString:',' param1:lastComma + 1]) > lastComma) {
-    NSString * numberPart = [smsURIWithoutQuery substringFromIndex:lastComma + 1 param1:comma];
+  NSMutableArray * numbers = [NSMutableArray arrayWithCapacity:1];
+  NSMutableArray * vias = [NSMutableArray arrayWithCapacity:1];
+  while ((comma = [smsURIWithoutQuery rangeOfString:@"," options:NSLiteralSearch range:NSMakeRange(lastComma + 1, [smsURIWithoutQuery length] - lastComma - 1)].location) > lastComma) {
+    NSString * numberPart = [smsURIWithoutQuery substringWithRange:NSMakeRange(lastComma + 1, [smsURIWithoutQuery length] - comma)];
     [self addNumberVia:numbers vias:vias numberPart:numberPart];
     lastComma = comma;
   }
-
   [self addNumberVia:numbers vias:vias numberPart:[smsURIWithoutQuery substringFromIndex:lastComma + 1]];
-  return [[[SMSParsedResult alloc] init:[self toStringArray:numbers] param1:[self toStringArray:vias] param2:subject param3:body] autorelease];
+
+  return [[[SMSParsedResult alloc] initWithNumbers:[self toStringArray:numbers]
+                                              vias:[self toStringArray:vias]
+                                           subject:subject
+                                              body:body] autorelease];
 }
 
 + (void) addNumberVia:(NSMutableArray *)numbers vias:(NSMutableArray *)vias numberPart:(NSString *)numberPart {
-  int numberEnd = [numberPart rangeOfString:';'];
+  int numberEnd = [numberPart rangeOfString:@";"].location;
   if (numberEnd < 0) {
     [numbers addObject:numberPart];
     [vias addObject:nil];
-  }
-   else {
-    [numbers addObject:[numberPart substringFromIndex:0 param1:numberEnd]];
+  } else {
+    [numbers addObject:[numberPart substringToIndex:numberEnd]];
     NSString * maybeVia = [numberPart substringFromIndex:numberEnd + 1];
     NSString * via;
     if ([maybeVia hasPrefix:@"via="]) {
       via = [maybeVia substringFromIndex:4];
-    }
-     else {
+    } else {
       via = nil;
     }
     [vias addObject:via];
