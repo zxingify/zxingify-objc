923dcf0bd5f17ccf0dd11eddfb30b1193f045187
Costa Walcott <costa@scvngr.com>
2012-02-16 10:43:57 -0500

    Port VEventResultParser

--- ZXingObjC/client/result/VEventResultParser.m
+++ ZXingObjC/client/result/VEventResultParser.m
@@ -1,50 +1,49 @@
+#import "CalendarParsedResult.h"
+#import "Result.h"
+#import "VCardResultParser.h"
 #import "VEventResultParser.h"
 
 @implementation VEventResultParser
 
-- (id) init {
-  if (self = [super init]) {
-  }
-  return self;
-}
-
 + (CalendarParsedResult *) parse:(Result *)result {
-  NSString * rawText = [result text];
+  NSString * rawText = result.text;
   if (rawText == nil) {
     return nil;
   }
-  int vEventStart = [rawText rangeOfString:@"BEGIN:VEVENT"];
+  int vEventStart = [rawText rangeOfString:@"BEGIN:VEVENT"].location;
   if (vEventStart < 0) {
     return nil;
   }
-  NSString * summary = [VCardResultParser matchSingleVCardPrefixedField:@"SUMMARY" param1:rawText param2:YES];
-  NSString * start = [VCardResultParser matchSingleVCardPrefixedField:@"DTSTART" param1:rawText param2:YES];
-  NSString * end = [VCardResultParser matchSingleVCardPrefixedField:@"DTEND" param1:rawText param2:YES];
-  NSString * location = [VCardResultParser matchSingleVCardPrefixedField:@"LOCATION" param1:rawText param2:YES];
-  NSString * description = [VCardResultParser matchSingleVCardPrefixedField:@"DESCRIPTION" param1:rawText param2:YES];
-  NSString * geoString = [VCardResultParser matchSingleVCardPrefixedField:@"GEO" param1:rawText param2:YES];
+
+  NSString * summary = [VCardResultParser matchSingleVCardPrefixedField:@"SUMMARY" rawText:rawText trim:YES];
+  NSString * start = [VCardResultParser matchSingleVCardPrefixedField:@"DTSTART" rawText:rawText trim:YES];
+  NSString * end = [VCardResultParser matchSingleVCardPrefixedField:@"DTEND" rawText:rawText trim:YES];
+  NSString * location = [VCardResultParser matchSingleVCardPrefixedField:@"LOCATION" rawText:rawText trim:YES];
+  NSString * description = [VCardResultParser matchSingleVCardPrefixedField:@"DESCRIPTION" rawText:rawText trim:YES];
+
+  NSString * geoString = [VCardResultParser matchSingleVCardPrefixedField:@"GEO" rawText:rawText trim:YES];
   double latitude;
   double longitude;
   if (geoString == nil) {
-    latitude = Double.NaN;
-    longitude = Double.NaN;
-  }
-   else {
-    int semicolon = [geoString rangeOfString:';'];
-
-    @try {
-      latitude = [Double parseDouble:[geoString substringFromIndex:0 param1:semicolon]];
-      longitude = [Double parseDouble:[geoString substringFromIndex:semicolon + 1]];
-    }
-    @catch (NumberFormatException * nfe) {
-      return nil;
-    }
+    latitude = NAN;
+    longitude = NAN;
+  } else {
+    int semicolon = [geoString rangeOfString:@";"].location;
+    latitude = [[geoString substringToIndex:semicolon] doubleValue];
+    longitude = [[geoString substringFromIndex:semicolon + 1] doubleValue];
   }
 
   @try {
-    return [[[CalendarParsedResult alloc] init:summary param1:start param2:end param3:location param4:nil param5:description param6:latitude param7:longitude] autorelease];
+    return [[[CalendarParsedResult alloc] initWithSummary:summary
+                                                    start:start
+                                                      end:end
+                                                 location:location
+                                                 attendee:nil
+                                              description:description
+                                                 latitude:latitude
+                                                longitude:longitude] autorelease];
   }
-  @catch (IllegalArgumentException * iae) {
+  @catch (NSException * iae) {
     return nil;
   }
 }
