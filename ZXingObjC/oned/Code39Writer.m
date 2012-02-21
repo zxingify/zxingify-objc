f48d66cbc954c243053fa62aa33a719d0f19c769
Costa Walcott <cwalcott@draconis.com>
2012-02-20 21:28:46 -0500

    Another update to Code39Writer

--- ZXingObjC/oned/Code39Writer.m
+++ ZXingObjC/oned/Code39Writer.m
@@ -4,7 +4,7 @@
 
 @interface Code39Writer ()
 
-- (void) toIntArray:(int)a toReturn:(NSArray *)toReturn;
+- (void) toIntArray:(int)a toReturn:(int[])toReturn;
 
 @end
 
@@ -25,21 +25,20 @@
                 format:@"Requested contents should be less than 80 digits long, but got %d", length];
   }
 
-  NSMutableArray * widths = [NSMutableArray arrayWithCapacity:9];
+  int widths[9];
   int codeWidth = 24 + 1 + length;
   for (int i = 0; i < length; i++) {
     int indexInString = [ALPHABET_STRING rangeOfString:[contents substringWithRange:NSMakeRange(i, 1)]].location;
     [self toIntArray:CHARACTER_ENCODINGS[indexInString] toReturn:widths];
-
-    for (int j = 0; j < [widths count]; j++) {
-      codeWidth += [[widths objectAtIndex:j] intValue];
+    for (int j = 0; j < sizeof(widths) / sizeof(int); j++) {
+      codeWidth += widths[j];
     }
   }
 
   NSMutableArray * result = [NSMutableArray arrayWithCapacity:codeWidth];
   [self toIntArray:CHARACTER_ENCODINGS[39] toReturn:widths];
   int pos = [Code39Writer appendPattern:result pos:0 pattern:widths startColor:1];
-  NSArray * narrowWhite = [NSArray arrayWithObject:[NSNumber numberWithInt:1]];
+  int narrowWhite[1];
   pos += [Code39Writer appendPattern:result pos:pos pattern:narrowWhite startColor:0];
 
   for (int i = length - 1; i >= 0; i--) {
@@ -54,10 +53,10 @@
   return result;
 }
 
-- (void) toIntArray:(int)a toReturn:(NSMutableArray *)toReturn {
+- (void) toIntArray:(int)a toReturn:(int[])toReturn {
   for (int i = 0; i < 9; i++) {
     int temp = a & (1 << i);
-    [toReturn addObject:[NSNumber numberWithInt:temp == 0 ? 1 : 2]];
+    toReturn[i] = temp == 0 ? 1 : 2;
   }
 }
 
