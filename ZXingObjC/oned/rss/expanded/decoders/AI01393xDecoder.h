b570d0f902a345aa68bc28b3a5961e68422be272
Costa Walcott <costa@scvngr.com>
2012-02-21 14:46:49 -0500

    Port AI01393xDecoder

--- ZXingObjC/oned/rss/expanded/decoders/AI01393xDecoder.h
+++ ZXingObjC/oned/rss/expanded/decoders/AI01393xDecoder.h
@@ -1,14 +1,9 @@
 #import "AI01decoder.h"
-#import "BitArray.h"
-#import "NotFoundException.h"
 
 /**
  * @author Pablo Orduña, University of Deusto (pablo.orduna@deusto.es)
  */
 
 @interface AI01393xDecoder : AI01decoder
-
-- (id) initWithInformation:(BitArray *)information;
-- (NSString *) parseInformation;
 
 @end
--- ZXingObjC/oned/rss/expanded/decoders/AI01393xDecoder.m
+++ ZXingObjC/oned/rss/expanded/decoders/AI01393xDecoder.m
@@ -1,4 +1,8 @@
 #import "AI01393xDecoder.h"
+#import "BitArray.h"
+#import "DecodedInformation.h"
+#import "GeneralAppIdDecoder.h"
+#import "NotFoundException.h"
 
 int const headerSize = 5 + 1 + 2;
 int const lastDigitSize = 2;
@@ -6,33 +10,32 @@ int const firstThreeDigitsSize = 10;
 
 @implementation AI01393xDecoder
 
-- (id) initWithInformation:(BitArray *)information {
-  if (self = [super init:information]) {
-  }
-  return self;
-}
-
 - (NSString *) parseInformation {
   if (information.size < headerSize + gtinSize) {
     @throw [NotFoundException notFoundInstance];
   }
-  NSMutableString * buf = [[[NSMutableString alloc] init] autorelease];
-  [self encodeCompressedGtin:buf param1:headerSize];
-  int lastAIdigit = [generalDecoder extractNumericValueFromBitArray:headerSize + gtinSize param1:lastDigitSize];
-  [buf append:@"(393"];
-  [buf append:lastAIdigit];
-  [buf append:')'];
-  int firstThreeDigits = [generalDecoder extractNumericValueFromBitArray:headerSize + gtinSize + lastDigitSize param1:firstThreeDigitsSize];
+
+  NSMutableString * buf = [NSMutableString string];
+
+  [self encodeCompressedGtin:buf currentPos:headerSize];
+
+  int lastAIdigit = [generalDecoder extractNumericValueFromBitArray:headerSize + gtinSize bits:lastDigitSize];
+
+  [buf appendFormat:@"(393%d)", lastAIdigit];
+
+  int firstThreeDigits = [generalDecoder extractNumericValueFromBitArray:headerSize + gtinSize + lastDigitSize bits:firstThreeDigitsSize];
   if (firstThreeDigits / 100 == 0) {
-    [buf append:'0'];
+    [buf appendString:@"0"];
   }
   if (firstThreeDigits / 10 == 0) {
-    [buf append:'0'];
+    [buf appendString:@"0"];
   }
-  [buf append:firstThreeDigits];
-  DecodedInformation * generalInformation = [generalDecoder decodeGeneralPurposeField:headerSize + gtinSize + lastDigitSize + firstThreeDigitsSize param1:nil];
-  [buf append:[generalInformation newString]];
-  return [buf description];
+  [buf appendFormat:@"%d", firstThreeDigits];
+
+  DecodedInformation * generalInformation = [generalDecoder decodeGeneralPurposeField:headerSize + gtinSize + lastDigitSize + firstThreeDigitsSize remaining:nil];
+  [buf appendString:generalInformation.theNewString];
+
+  return buf;
 }
 
 @end
