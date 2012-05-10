7bca7f8c51cf83b5e63b503f17588c4c44788132
Costa Walcott <cwalcott@gmail.com>
2012-05-10 17:17:17 -0400

    Added QR code blackbox 3 tests

---
+++ ZXingObjCTests/qrcode/QRCodeBlackBox3TestCase.h
@@ -0,0 +1,5 @@
+#import "AbstractBlackBoxTestCase.h"
+
+@interface QRCodeBlackBox3TestCase : AbstractBlackBoxTestCase
+
+@end
---
+++ ZXingObjCTests/qrcode/QRCodeBlackBox3TestCase.m
@@ -0,0 +1,27 @@
+#import "QRCodeBlackBox3TestCase.h"
+#import "ZXBarcodeFormat.h"
+#import "ZXMultiFormatReader.h"
+
+@implementation QRCodeBlackBox3TestCase
+
+- (id)initWithInvocation:(NSInvocation *)anInvocation {
+  self = [super initWithInvocation:anInvocation
+                testBasePathSuffix:@"Resources/blackbox/qrcode-3"
+                     barcodeReader:[[[ZXMultiFormatReader alloc] init] autorelease]
+                    expectedFormat:kBarcodeFormatQRCode];
+
+  if (self) {
+    [self addTest:38 tryHarderCount:38 rotation:0.0f];
+    [self addTest:38 tryHarderCount:38 rotation:90.0f];
+    [self addTest:36 tryHarderCount:36 rotation:180.0f];
+    [self addTest:38 tryHarderCount:38 rotation:270.0f];
+  }
+
+  return self;
+}
+
+- (void) testBlackBox {
+  [super runTests];
+}
+
+@end
