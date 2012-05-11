c6c137be4f7ed8cbd40e28da8f32d624a9e751d1
Costa Walcott <cwalcott@gmail.com>
2012-05-11 11:45:35 -0400

    Add qrcode blackbox 5 tests

---
+++ ZXingObjCTests/qrcode/QRCodeBlackBox5TestCase.h
@@ -0,0 +1,5 @@
+#import "AbstractBlackBoxTestCase.h"
+
+@interface QRCodeBlackBox5TestCase : AbstractBlackBoxTestCase
+
+@end
---
+++ ZXingObjCTests/qrcode/QRCodeBlackBox5TestCase.m
@@ -0,0 +1,27 @@
+#import "QRCodeBlackBox5TestCase.h"
+#import "ZXBarcodeFormat.h"
+#import "ZXMultiFormatReader.h"
+
+@implementation QRCodeBlackBox5TestCase
+
+- (id)initWithInvocation:(NSInvocation *)anInvocation {
+  self = [super initWithInvocation:anInvocation
+                testBasePathSuffix:@"Resources/blackbox/qrcode-5"
+                     barcodeReader:[[[ZXMultiFormatReader alloc] init] autorelease]
+                    expectedFormat:kBarcodeFormatQRCode];
+
+  if (self) {
+    [self addTest:18 tryHarderCount:18 rotation:0.0f];
+    [self addTest:19 tryHarderCount:19 rotation:90.0f];
+    [self addTest:19 tryHarderCount:19 rotation:180.0f];
+    [self addTest:18 tryHarderCount:18 rotation:270.0f];
+  }
+
+  return self;
+}
+
+- (void)testBlackBox {
+  [super runTests];
+}
+
+@end
