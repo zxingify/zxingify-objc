02e9c63db6473f6d2306f75438db5bf8ab24353b
Costa Walcott <cwalcott@draconis.com>
2012-05-13 00:22:43 -0400

    Code39 blackbox 3 test

---
+++ ZXingObjCTests/oned/Code39BlackBox3TestCase.h
@@ -0,0 +1,5 @@
+#import "AbstractBlackBoxTestCase.h"
+
+@interface Code39BlackBox3TestCase : AbstractBlackBoxTestCase
+
+@end
---
+++ ZXingObjCTests/oned/Code39BlackBox3TestCase.m
@@ -0,0 +1,25 @@
+#import "Code39BlackBox3TestCase.h"
+#import "ZXBarcodeFormat.h"
+#import "ZXMultiFormatReader.h"
+
+@implementation Code39BlackBox3TestCase
+
+- (id)initWithInvocation:(NSInvocation *)anInvocation {
+  self = [super initWithInvocation:anInvocation
+                testBasePathSuffix:@"Resources/blackbox/code39-3"
+                     barcodeReader:[[[ZXMultiFormatReader alloc] init] autorelease]
+                    expectedFormat:kBarcodeFormatCode39];
+
+  if (self) {
+    [self addTest:17 tryHarderCount:17 rotation:0.0f];
+    [self addTest:17 tryHarderCount:17 rotation:180.0f];
+  }
+
+  return self;
+}
+
+- (void)testBlackBox {
+  [super runTests];
+}
+
+@end
