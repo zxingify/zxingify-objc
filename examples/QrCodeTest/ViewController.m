944daade11fddeb8841b3a2b17b5275660ef3310
Costa Walcott <cwalcott@draconis.com>
2012-05-22 22:42:04 -0400

    Small update to example QrCodeTest

--- examples/QrCodeTest/ViewController.m
+++ examples/QrCodeTest/ViewController.m
@@ -41,39 +41,41 @@
 
 - (IBAction)updatePressed:(id)sender {
   NSString *data = self.textView.text;
-  [self.textView resignFirstResponder];
+  if (data && ![data isEqualToString:@""]) {
+    [self.textView resignFirstResponder];
 
-  self.imageView.image = nil;
+    self.imageView.image = nil;
 
-  ZXMultiFormatWriter* writer = [[ZXMultiFormatWriter alloc] init];
-  ZXBitMatrix* result = [writer encode:data format:kBarcodeFormatQRCode width:self.imageView.frame.size.width height:self.imageView.frame.size.width];
+    ZXMultiFormatWriter* writer = [[ZXMultiFormatWriter alloc] init];
+    ZXBitMatrix* result = [writer encode:data format:kBarcodeFormatQRCode width:self.imageView.frame.size.width height:self.imageView.frame.size.width];
 
-  int width = result.width;
-  int height = result.height;
-  unsigned char *bytes = (unsigned char *)malloc(width * height * 4);
-  for(int y = 0; y < height; y++) {
-    for(int x = 0; x < width; x++) {
-      BOOL bit = [result get:x y:y];
-      unsigned char intensity = bit ? 0 : 255;
-      for(int i = 0; i < 3; i++) {
-        bytes[y * width * 4 + x * 4 + i] = intensity;
+    int width = result.width;
+    int height = result.height;
+    unsigned char *bytes = (unsigned char *)malloc(width * height * 4);
+    for(int y = 0; y < height; y++) {
+      for(int x = 0; x < width; x++) {
+        BOOL bit = [result getX:x y:y];
+        unsigned char intensity = bit ? 0 : 255;
+        for(int i = 0; i < 3; i++) {
+          bytes[y * width * 4 + x * 4 + i] = intensity;
+        }
+        bytes[y * width * 4 + x * 4 + 3] = 255;
       }
-      bytes[y * width * 4 + x * 4 + 3] = 255;
     }
+
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef c = CGBitmapContextCreate(bytes, width, height, 8, 4 * width, colorSpace, kCGImageAlphaPremultipliedLast);
+    CFRelease(colorSpace);
+    CGImageRef image = CGBitmapContextCreateImage(c);
+    CFRelease(c);
+    UIImage *image2 = [UIImage imageWithCGImage:image];
+    CFRelease(image);
+
+    free(bytes);
+    self.imageView.image = image2;
+
+    [writer release];
   }
-
-  CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-  CGContextRef c = CGBitmapContextCreate(bytes, width, height, 8, 4 * width, colorSpace, kCGImageAlphaPremultipliedLast);
-  CFRelease(colorSpace);
-  CGImageRef image = CGBitmapContextCreateImage(c);
-  CFRelease(c);
-  UIImage *image2 = [UIImage imageWithCGImage:image];
-  CFRelease(image);
-
-  free(bytes);
-  self.imageView.image = image2;
-
-  [writer release];
 }
 
 @end
