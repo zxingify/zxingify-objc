8524fe2f9f28f6757acb6745c3f1e366a1ce3081
Costa Walcott <cwalcott@gmail.com>
2013-02-03 14:11:35 -0500

    Ported from ZXing: "Issue 1295 allow broader definition of  URI"

--- ZXingObjC/client/result/ZXURIResultParser.m
+++ ZXingObjC/client/result/ZXURIResultParser.m
@@ -18,32 +18,31 @@
 #import "ZXResult.h"
 #import "ZXURIParsedResult.h"
 
-static NSString* PATTERN_END =
-  @"(:\\d{1,5})?" // maybe port
-  @"(/|\\?|$)"; // query, path or nothing
-
-static NSRegularExpression* URL_WITH_PROTOCOL_PATTERN = nil;
-static NSRegularExpression* URL_WITHOUT_PROTOCOL_PATTERN = nil;
+static NSString * ALPHANUM_PART = @"[a-zA-Z0-9\\-]";
+static NSRegularExpression * URL_WITH_PROTOCOL_PATTERN = nil;
+static NSRegularExpression * URL_WITHOUT_PROTOCOL_PATTERN = nil;
 
 @implementation ZXURIResultParser
 
 + (void)initialize {
-  URL_WITH_PROTOCOL_PATTERN = [[NSRegularExpression alloc] initWithPattern:
-                               [@"^[a-zA-Z0-9]{2,}:(/)*" // protocol
-                                @"[a-zA-Z0-9\\-]+(\\.[a-zA-Z0-9\\-]+)*" // host name elements
-                                stringByAppendingString:PATTERN_END]
-                                                                   options:0 error:nil];
+  URL_WITH_PROTOCOL_PATTERN = [[NSRegularExpression alloc] initWithPattern:@"^[a-zA-Z0-9]{2,}:"
+                                                                   options:0
+                                                                     error:nil];
   URL_WITHOUT_PROTOCOL_PATTERN = [[NSRegularExpression alloc] initWithPattern:
-                                  [@"([a-zA-Z0-9\\-]+\\.)+[a-zA-Z0-9\\-]{2,}" // host name elements
-                                   stringByAppendingString:PATTERN_END]
-                                                                      options:0 error:nil];
+                                  [[[NSString stringWithFormat:@"(%@+\\.)+%@{2,}", ALPHANUM_PART, ALPHANUM_PART] // host name elements
+                                    stringByAppendingString:@"(:\\d{1,5})?"] // maybe port
+                                   stringByAppendingString:@"(/|\\?|$)"] // query, path or nothing
+                                                                      options:0
+                                                                        error:nil];
 }
 
 - (ZXParsedResult *)parse:(ZXResult *)result {
   NSString * rawText = [ZXResultParser massagedText:result];
-  // We specifically handle the odd "URL" scheme here for simplicity
-  if ([rawText hasPrefix:@"URL:"]) {
-    rawText = [rawText substringFromIndex:4];
+  // We specifically handle the odd "URL" scheme here for simplicity and add "URI" for fun
+  // Assume anything starting this way really means to be a URI
+  if ([rawText hasPrefix:@"URL:"] || [rawText hasPrefix:@"URI:"]) {
+    return [[ZXURIParsedResult alloc] initWithUri:[[rawText substringFromIndex:4] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]
+                                            title:nil];
   }
   rawText = [rawText stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
   return [[self class] isBasicallyValidURI:rawText] ? [ZXURIParsedResult uriParsedResultWithUri:rawText title:nil] : nil;
