1e0e322be7df79243d932675ffe596cc002cff66
Costa Walcott <cwalcott@gmail.com>
2013-10-02 23:18:06 -0400

    Ported r2619 from ZXing
    Original commit message: "Issue 1659 carry through metadata on multiple
    barcodes"

--- ZXingObjC/multi/ZXGenericMultipleBarcodeReader.m
+++ ZXingObjC/multi/ZXGenericMultipleBarcodeReader.m
@@ -127,7 +127,9 @@ int const MAX_DEPTH = 4;
     [newResultPoints addObject:[[ZXResultPoint alloc] initWithX:[oldPoint x] + xOffset y:[oldPoint y] + yOffset]];
   }
 
-  return [ZXResult resultWithText:result.text rawBytes:result.rawBytes length:result.length resultPoints:newResultPoints format:result.barcodeFormat];
+  ZXResult *newResult = [ZXResult resultWithText:result.text rawBytes:result.rawBytes length:result.length resultPoints:newResultPoints format:result.barcodeFormat];
+  [newResult putAllMetadata:result.resultMetadata];
+  return newResult;
 }
 
 @end
