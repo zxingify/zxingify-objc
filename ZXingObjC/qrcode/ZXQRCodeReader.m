664b0affc1244efc08f37beffc5fe7d0a6ab684b
Costa Walcott <cwalcott@gmail.com>
2013-10-03 00:22:58 -0400

    Ported r2679 from ZXing
    Original commit message: "Issue 1138 reject image with nonsensical
    right-bottom black pixel in pure barcode mode"

--- ZXingObjC/qrcode/ZXQRCodeReader.m
+++ ZXingObjC/qrcode/ZXQRCodeReader.m
@@ -116,6 +116,11 @@
   int left = [leftTopBlack[0] intValue];
   int right = [rightBottomBlack[0] intValue];
 
+  // Sanity check!
+  if (left >= right || top >= bottom) {
+    return nil;
+  }
+
   if (bottom - top != right - left) {
     // Special case, where bottom-right module wasn't black so we found something else in the last row
     // Assume it's a square, so use height as the width
