21d9790ec9bf1c468c60b6eab086610448f4f7f2
Emil Marashliev <emarashliev@me.com>
2014-01-08 15:46:14 +0200

    Fixed possible crashes when Modal ViewControler conforms to ZXCaptureDelegate.

--- ZXingObjC/client/ZXCapture.m
+++ ZXingObjC/client/ZXCapture.m
@@ -53,9 +53,9 @@ static bool isIPad();
 #   define KS_DISPATCH_RELEASE(q)
 #endif
 
-@interface ZXCapture ()
-
-@property (nonatomic, assign) dispatch_queue_t captureQueue;
+@interface ZXCapture (){
+    dispatch_queue_t _captureQueue;
+}
 
 @end
 
@@ -323,7 +323,7 @@ static bool isIPad();
                 ZXAV(setAlwaysDiscardsLateVideoFrames:)YES];
 
     [output ZXQT(setDelegate:)ZXAV(setSampleBufferDelegate:)self
-                  ZXAV(queue:self.captureQueue)];
+                  ZXAV(queue:_captureQueue)];
 
     [self.session addOutput:output ZXQT(error:nil)];
   }
@@ -494,19 +494,6 @@ static bool isIPad();
     NSLog(@"afl %@ %@", _layer, event);
   }
   return nil;
-}
-
-- (void)dealloc {
-  if (input && session) {
-    [session removeInput:input];
-  }
-  if (output && session) {
-    [session removeOutput:output];
-  }
-  if (_captureQueue) {
-    KS_DISPATCH_RELEASE(_captureQueue);
-    _captureQueue = nil;
-  }
 }
 
 - (void)captureOutput:(ZXCaptureOutput *)captureOutput
