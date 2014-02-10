9cccd2e002dd6b7981b5e930ee1116204c1d1fe5
stephenzl <stephenxtddd@gmail.com>
2014-02-10 19:25:34 +0800

    Update ZXCapture.m

--- ZXingObjC/client/ZXCapture.m
+++ ZXingObjC/client/ZXCapture.m
@@ -521,6 +521,7 @@ ZXAV(didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer)
     // NSLog(@"received frame");
 
     ZXAV(CVImageBufferRef videoFrame = CMSampleBufferGetImageBuffer(sampleBuffer));
+    ZXAV(UIImage *frameImage = [self imageFromSampleBuffer:sampleBuffer]);
 
     // NSLog(@"%ld %ld", CVPixelBufferGetWidth(videoFrame), CVPixelBufferGetHeight(videoFrame));
     // NSLog(@"delegate %@", delegate);
@@ -602,13 +603,62 @@ ZXAV(didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer)
         ZXResult *result = [self.reader decode:bitmap hints:hints error:&error];
         if (result) {
           dispatch_async(dispatch_get_main_queue(), ^{
-            [delegate captureResult:self result:result];
+              if ([delegate respondsToSelector:@selector(captureResult:result:)]) {
+                  
+                  [delegate captureResult:self result:result];
+                  
+              } else if ([delegate respondsToSelector:@selector(captureResult:result:frameImage:)]) {
+                  
+                  [delegate captureResult:self result:result frameImage:frameImage];
+              }
           });
         }
       }
     }
   }
 }
+
+- (UIImage *)imageFromSampleBuffer:(CMSampleBufferRef)sampleBuffer
+{
+    // Get a CMSampleBuffer's Core Video image buffer for the media data
+    CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    // Lock the base address of the pixel buffer
+    CVPixelBufferLockBaseAddress(imageBuffer, 0);
+    
+    // Get the number of bytes per row for the pixel buffer
+    void *baseAddress = CVPixelBufferGetBaseAddress(imageBuffer);
+    
+    // Get the number of bytes per row for the pixel buffer
+    size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer);
+    // Get the pixel buffer width and height
+    size_t w = CVPixelBufferGetWidth(imageBuffer);
+    size_t h = CVPixelBufferGetHeight(imageBuffer);
+    
+    // Create a device-dependent RGB color space
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    // Create a bitmap graphics context with the sample buffer data
+    CGContextRef context = CGBitmapContextCreate(baseAddress, w, h, 8,
+                                                 bytesPerRow, colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
+    // Create a Quartz image from the pixel data in the bitmap graphics context
+    CGImageRef quartzImage = CGBitmapContextCreateImage(context);
+    // Unlock the pixel buffer
+    CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
+    
+    // Free up the context and color space
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    
+    // Create an image object from the Quartz image
+    //UIImage *image = [UIImage imageWithCGImage:quartzImage];
+    UIImage *image = [UIImage imageWithCGImage:quartzImage scale:1.0f orientation:UIImageOrientationRight];
+    
+    // Release the Quartz image
+    CGImageRelease(quartzImage);
+    
+    return (image);
+}
+
 
 - (BOOL)hasFront {
   NSArray *devices = 
