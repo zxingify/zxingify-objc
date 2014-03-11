1de33725140225cbb61a238688204a544453ae1d
Costa Walcott <cwalcott@gmail.com>
2014-03-11 10:09:48 -0400

    Cleanup the QrCodeTest code a bit

--- examples/QrCodeTest/ViewController.m
+++ examples/QrCodeTest/ViewController.m
@@ -23,25 +23,29 @@
 - (void)viewWillAppear:(BOOL)animated {
   [super viewWillAppear:animated];
 
-  // Default qr data
-  self.textView.text = @"http://code.google.com/p/zxing/";
+  self.textView.text = @"http://github.com/TheLevelUp/ZXingObjC";
   [self updatePressed:nil];
 }
 
 #pragma mark - Events
 
 - (IBAction)updatePressed:(id)sender {
-  NSString *data = self.textView.text;
-  if (data && ![data isEqualToString:@""]) {
-    [self.textView resignFirstResponder];
+  [self.textView resignFirstResponder];
 
-    ZXMultiFormatWriter *writer = [[ZXMultiFormatWriter alloc] init];
-    ZXBitMatrix *result = [writer encode:data format:kBarcodeFormatQRCode width:self.imageView.frame.size.width height:self.imageView.frame.size.width error:nil];
-    if (result) {
-      self.imageView.image = [UIImage imageWithCGImage:[ZXImage imageWithMatrix:result].cgimage];
-    } else {
-      self.imageView.image = nil;
-    }
+  NSString *data = self.textView.text;
+  if (data == 0) return;
+
+  ZXMultiFormatWriter *writer = [[ZXMultiFormatWriter alloc] init];
+  ZXBitMatrix *result = [writer encode:data
+                                format:kBarcodeFormatQRCode
+                                 width:self.imageView.frame.size.width
+                                height:self.imageView.frame.size.width
+                                 error:nil];
+
+  if (result) {
+    self.imageView.image = [UIImage imageWithCGImage:[ZXImage imageWithMatrix:result].cgimage];
+  } else {
+    self.imageView.image = nil;
   }
 }
 
