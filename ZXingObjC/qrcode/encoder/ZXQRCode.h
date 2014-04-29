SinnerSchraderMobileMirrors/ZXingObjC

Update comments to better match Javadocs from ZXing
Also rename constants to include "ZX_" prefix

ZXingObjC/qrcode/encoder/ZXQRCode.h
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-extern int const NUM_MASK_PATTERNS;
+extern const int NUM_MASK_PATTERNS;
 
 @class ZXByteMatrix, ZXErrorCorrectionLevel, ZXMode, ZXQRCodeVersion;
 

zxingify/zxingify-objc

ZXingObjC/qrcode/encoder/ZXQRCode.h
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-extern int const NUM_MASK_PATTERNS;
+extern const int ZX_NUM_MASK_PATTERNS;
 
 @class ZXByteMatrix, ZXErrorCorrectionLevel, ZXMode, ZXQRCodeVersion;
 