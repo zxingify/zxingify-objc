f4d76105a17b4b34bbb75795643d962d01391213
Carlos Vidal <nakioparkour@gmail.com>
2015-10-06 11:35:47 +0100

    unit tested ZXGenericMultipleBarcodeReader

--- ZXingObjCTests/multi/ZXGenericMultipleBarcodeReaderTestCase.m
+++ ZXingObjCTests/multi/ZXGenericMultipleBarcodeReaderTestCase.m
@@ -1,39 +1,51 @@
-//
-//  ZXGenericMultipleBarcodeReaderTestCase.m
-//  ZXingObjC
-//
-//  Created by Carlos Vidal Pallin on 06/10/2015.
-//  Copyright © 2015 zxing. All rights reserved.
-//
+/*
+ * Copyright 2012 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-#import <XCTest/XCTest.h>
+#import "ZXMultiAbstractBlackBoxTestCase.h"
 
-@interface ZXGenericMultipleBarcodeReaderTestCase : XCTestCase
+@interface ZXGenericMultipleBarcodeReaderTestCase : ZXMultiAbstractBlackBoxTestCase
+
+@property (nonatomic, strong) ZXMultiFormatReader *reader;
 
 @end
 
 @implementation ZXGenericMultipleBarcodeReaderTestCase
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (id)initWithInvocation:(NSInvocation *)invocation {
+    ZXMultiFormatReader *reader = [ZXMultiFormatReader reader];
+    ZXGenericMultipleBarcodeReader *multiReader = [[ZXGenericMultipleBarcodeReader alloc] initWithDelegate:reader];
+    NSArray *expectedFormats = @[@(kBarcodeFormatCode39), @(kBarcodeFormatCode128), @(kBarcodeFormatEan13), @(kBarcodeFormatQRCode), @(kBarcodeFormatUPCE)];
+    self = [super initWithInvocation:invocation
+                  testBasePathSuffix:@"Resources/blackbox/multi"
+               multipleBarcodeReader:multiReader
+                     expectedFormats:expectedFormats
+            ];
+    
+    if (self) {
+        _reader = reader;
+        [self addTest:1 tryHarderCount:1 rotation:0.0f];
+        [self addTest:1 tryHarderCount:1 rotation:90.0f];
+        [self addTest:1 tryHarderCount:1 rotation:180.0f];
+    }
+    
+    return self;
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testBlackBox {
+    [super runTests];
 }
 
 @end
