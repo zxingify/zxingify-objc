2aebab44b01dd0ebafbd2f796d57f4d8415eedcd
Ben John <github@benjohn.de>
2018-09-21 13:30:52 +0200

    Ported 1a5b739cdc7a01d4f943ebd962729ad949b97d2e

--- ZXingObjC/qrcode/ZXQRCodeReader.m
+++ ZXingObjC/qrcode/ZXQRCodeReader.m
@@ -142,6 +142,10 @@
     // Special case, where bottom-right module wasn't black so we found something else in the last row
     // Assume it's a square, so use height as the width
     right = left + (bottom - top);
+    if (right >= image.width) {
+      // Abort if that would not make sense -- off image
+      return nil;
+    }
   }
 
   int matrixWidth = round((right - left + 1) / moduleSize);
