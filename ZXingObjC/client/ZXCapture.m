11170d28cbd6fbfcfed9abe797fd337460ab9f22
kenstir <kenstir@gmail.com>
2023-04-08 21:23:43 -0400

    Replace deprecated function AVCaptureDevice devicesWithMediaType:

--- ZXingObjC/client/ZXCapture.m
+++ ZXingObjC/client/ZXCapture.m
@@ -211,12 +211,18 @@
 }
 
 - (BOOL)hasFront {
-  NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
-  return [devices count] > 1;
+  AVCaptureDeviceDiscoverySession *captureDeviceDiscoverySession = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:@[AVCaptureDeviceTypeBuiltInWideAngleCamera]
+    mediaType:AVMediaTypeVideo
+    position:AVCaptureDevicePositionFront];
+  NSArray *devices = [captureDeviceDiscoverySession devices];
+  return [devices count] > 0;
 }
 
 - (BOOL)hasBack {
-  NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+  AVCaptureDeviceDiscoverySession *captureDeviceDiscoverySession = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:@[AVCaptureDeviceTypeBuiltInWideAngleCamera]
+    mediaType:AVMediaTypeVideo
+    position:AVCaptureDevicePositionBack];
+  NSArray *devices = [captureDeviceDiscoverySession devices];
   return [devices count] > 0;
 }
 
@@ -547,8 +553,11 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
   
   AVCaptureDevice *zxd = nil;
   
-  NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
-  
+  AVCaptureDeviceDiscoverySession *captureDeviceDiscoverySession = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:@[AVCaptureDeviceTypeBuiltInWideAngleCamera]
+    mediaType:AVMediaTypeVideo
+    position:AVCaptureDevicePositionUnspecified];
+  NSArray *devices = [captureDeviceDiscoverySession devices];
+
   if ([devices count] > 0) {
     if (self.captureDeviceIndex == -1) {
       AVCaptureDevicePosition position = AVCaptureDevicePositionBack;
